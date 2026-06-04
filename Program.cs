4c4ee0fb123116c0ca2711e8a768ec783f292e69
sm-lucas <133544611+sm-lucas@users.noreply.github.com>
2026-06-03 22:33:25 -0300

    setup: criação do modelo de dados para ordens de serviço e propriedades de formatação

--- Program.cs
+++ Program.cs
@@ -2,3 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
+namespace ControleOrdensServico
+{
+    // Modelo da Ordem de Serviço (OS)
+    class OrdemServico
+    {
+        public int Id { get; set; }
+        public string Equipamento { get; set; }
+        public string DescricaoDefeito { get; set; }
+        public string Status { get; set; } // Aberto, Em Andamento, Concluído
+        public DateTime DataAbertura { get; set; }
+
+        public override string ToString()
+        {
+            string statusFormatado = Status.PadRight(13);
+            return $"OS: {Id:D3} | Status: [{statusFormatado}] | Equipamento: {Equipamento.PadRight(15)} | Defeito: {DescricaoDefeito}";
+        }
+    }
